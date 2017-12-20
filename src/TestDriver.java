dd5a77723259e27f67b04b757e6e98c819e91866
Shriram Varadarajan <shriram.varadarajan@gmail.com>
2017-12-20 10:21:49 -0600

    Tried to import a manual package

--- src/TestDriver.java
+++ src/TestDriver.java
@@ -1,5 +1,8 @@
+import shapes.*;
 public class TestDriver {
     public static void main(String[] args) {
         System.out.println("s");
+        Rectangle rec = new Rectangle();
+        rec.setArea();
     }
 }
